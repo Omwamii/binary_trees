76577c0c0169913238dfbb80088ad582f0f866c5
omwami <ianalpha@protonmail.com>
2023-06-06 23:49:47 +0300

    rotate tree right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,17 +8,26 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *new_root;
+	binary_tree_t *new_root, *pops;
 
-	if (!tree)
-		return (NULL);
-	if (!tree->left && !tree->right)
+	if (!tree || tree->left)
 		return (NULL);
 
+	pops = tree->parent;
 	new_root = tree->left;
 	tree->left = new_root->right;
-	new_root->parent = tree->parent;
+	if (new_root->right)
+		new_root->right->parent = tree;
+	new_root->parent = pops;
 	new_root->right = tree;
 	tree->parent = new_root;
+	if (pops)
+	{
+		/* adjust original parent whether left or right sub-tree */
+		if (pops->left == tree)
+			pops->left = new_root;
+		else
+			pops->right = new_root;
+	}
 	return (new_root);
 }
