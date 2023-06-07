c320e2fcd5ad9efa1fbfb05da2d6322a558a220b
omwami <ianalpha@protonmail.com>
2023-06-07 18:13:33 +0300

    BST insertion

---
+++ 111-bst_insert.c
@@ -0,0 +1,56 @@
+#include "binary_trees.h"
+
+bst_t *find_parent(bst_t *node, int value);
+/**
+ * bst_insert - insert into binary search tree
+ * @tree: ptr to ptr to root node
+ * @value: value to store in node to be inserted
+ *
+ * Return: ptr to created node, NULL on fail
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *pops, *root, *new_node;
+
+	if (!tree)
+		return (NULL);
+	root = *tree;
+	pops = find_parent(root, value);
+	new_node = (bst_t *)binary_tree_node(pops, value);
+	if (pops)
+	{
+		if (!pops->left && pops->n > value)
+			pops->left = new_node;
+		else
+			pops->right = new_node;
+	}
+	else
+	{
+		if (!(*tree))
+			*tree = new_node;
+		else
+			return (NULL); /* duplicate value found */
+	}
+	return (new_node);
+}
+
+/**
+ * find_parent - recursively find appropriate parent
+ * @node: root node of subtree
+ * @value: value of node to insert
+ *
+ * Return: parent node
+ */
+bst_t *find_parent(bst_t *node, int value)
+{
+	if (!node || node->n == value)
+		return (NULL);
+	if ((!node->left && node->n > value) ||
+			(!node->right && node->n < value))
+		return (node);
+	if (value > node->n)
+		return (find_parent(node->right, value));
+	if (value < node->n)
+		return (find_parent(node->left, value));
+	return (node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -19,7 +19,7 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+} binary_tree_t, bst_t, avl_t, heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -48,5 +48,6 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif
